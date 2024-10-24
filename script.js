9eb414e2dd625d8afd7ad2530d72e9813c5d1262
anupammaiti10 <maitianupam567@gmail.com>
2024-10-24 21:19:47 +0530

    1.script.js: a) click on the windows_popup container then the popup is not closed and b) hover effect still have untill the windows_popup is closed 2.style.css: just space is modified

--- script.js
+++ script.js
@@ -13,30 +13,34 @@ setInterval(updateTimeAndDate, 1000);
 // Initial call to set the values right away
 updateTimeAndDate();
 
-// Open the popup window on clicking the Windows icon-----
+// Open the popup window on clicking the Windows icon----------------------------------------------------------------
 let clickCount = 0;
 var windows_popup=document.querySelector(".windows_popup");
 var fa_windows=document.querySelector(".fa-windows");
 var icons=document.querySelector(".icons");
 var windows=document.querySelector(".windows");
- var value=0;
+const hover=document.querySelector(".hover");
+var value=0;
+
  icons.addEventListener("click", function open_popup() {
     clickCount++; // Increment the click count for tracking
-
     // Add the transition class every time the icon is clicked
     fa_windows.classList.add("transition_icons");
-
     // Check if the windows_popup is currently open or closed
     if (value === 0 && (clickCount % 2 !== 0)) {
         // If it's closed and we have an odd number of clicks, open the popup
         windows_popup.classList.add("open_popup");
+        hover.style.background='rgb(77, 106, 103)';
+        hover.style.borderRadius='5px';
+        // windows.setAttribute(".hover" );
         value = 1; // Set value to indicate popup is open
     } else {
         // If it's open, close the popup
         windows_popup.classList.remove("open_popup");
+        hover.style.background='';
+        hover.style.borderRadius='';
         value = 0; // Set value to indicate popup is closed
     }
-
     // Optionally, you may want to remove the transition class after a delay
     // This ensures that the class is removed after the transition effect.
     setTimeout(() => {
@@ -44,24 +48,29 @@ var windows=document.querySelector(".windows");
     }, 300); // Adjust the timeout duration (300ms) to match your CSS transition duration
 });
 
-//This code is on if i click on the windows button then the popup eill be hidden-----
+//This code is on if i click on the windows button then the popup will be hidden----------------------------------------------------------------
 
 // windows.addEventListener('click', function(event) {
 //     if (event.target == windows && windows_popup.classList.contains("open_popup")) {
 //         windows_popup.classList.remove('open_popup');
 //     }
 // });
-windows.addEventListener('click', function(event) {
+windows.addEventListener('click', function close(event) {
     // Check if the clicked element is not the popup or its children
     if (!windows_popup.contains(event.target)) {
         // Close the popup if it's open
         if (windows_popup.classList.contains("open_popup")) {
             windows_popup.classList.remove('open_popup');
+            hover.style.background='';
+            hover.style.borderRadius='';
         }
     }
 });
-//the following code clicking on the wifi_volu_battery button-----
-
+//This function based on if i click on the windows_popup container then the popup is not closed----------------------------------------------------------------
+windows_popup.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+//The following code clicking on the wifi_volu_battery button-----------------------------------------------------------------------
 let isOpen = false; // Change to a more descriptive variable name
 var wifi_vol_battery = document.querySelector(".wifi-volu-battery");
 var container = document.querySelector(".container");
@@ -76,7 +85,7 @@ wifi_vol_battery.addEventListener("click", function() {
     }
     
 });
-
+// This function based on the task_view container ----------------------------------------------------------------
 var task_view=document.querySelector(".task_view");
 var desktop_container=document.querySelector(".desktop-container");
 task_view.addEventListener("mouseover", function (e){
@@ -85,7 +94,6 @@ task_view.addEventListener("mouseover", function (e){
     }
      
 });
-
 task_view.addEventListener("mouseout", function(e) {
     if (e.target !== task_view) {
         desktop_container.classList.remove("open_popup2");
